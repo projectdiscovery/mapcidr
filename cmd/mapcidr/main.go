b2e2e25842ff2e42f9b7a9bc455ebc9c8a7dd30a
sandeep <8293321+ehsandeep@users.noreply.github.com>
2022-09-13 04:18:15 +0530

    version update

--- cmd/mapcidr/main.go
+++ cmd/mapcidr/main.go
@@ -54,12 +54,12 @@ const banner = `
                    ____________  ___    
   __ _  ___ ____  / ___/  _/ _ \/ _ \   
  /  ' \/ _ '/ _ \/ /___/ // // / , _/   
-/_/_/_/\_,_/ .__/\___/___/____/_/|_| v1.0.1
+/_/_/_/\_,_/ .__/\___/___/____/_/|_| v1.0.2
           /_/                                                     	 
 `
 
 // Version is the current version of mapcidr
-const Version = `v1.0.1`
+const Version = `v1.0.2`
 
 // showBanner is used to show the banner to the user
 func showBanner() {
