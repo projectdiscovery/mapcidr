28d56d4f2733e547ae5f0ebe374a8632df743644
GitHub Action <action@github.com>
2024-09-08 23:55:45 +0000

    Bump version

--- cmd/mapcidr/main.go
+++ cmd/mapcidr/main.go
@@ -77,7 +77,7 @@ const banner = `
 `
 
 // Version is the current version of mapcidr
-const version = `v1.1.55`
+const version = `v1.1.56`
 
 // showBanner is used to show the banner to the user
 func showBanner() {
